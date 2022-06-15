ed4aac7d596f810514d93fb3e9cda01db6c005e7
ali <a_icam@yahoo.com>
2022-06-15 23:35:42 +0430

    matching score

---
+++ src/score_match.py
@@ -0,0 +1,48 @@
+import os
+import json
+import numpy as np
+
+prepositions = ['از', 'به', 'در', 'با']
+
+'''
+    This class provides functionalities to get weight of each word in scoring answers
+    vocabDict is initiated each time server ran, if there is a vocab.json it is loaded, otherwise it is created
+'''
+class ScoreMatch:
+    def __init__(self):
+        self.vocabCount = 0
+        self.vocabDict = {}
+
+        if os.path.exists("vocabs.json"):
+            with open('vocabs.json', 'r') as openfile:
+                self.vocabDict = json.load(openfile)
+
+    def score_vocab(self, vocab):
+        if vocab not in self.vocabDict.keys():
+            return 1/len(self.vocabDict.keys())
+        return self.vocabDict[vocab] / np.sum(self.vocabDict.values())
+
+    def store_sentence(self, sentence):
+        vocabs = sentence.split(' ')
+        for vocab in vocabs:
+            if vocab not in prepositions:
+                if vocab in self.vocabDict.keys():
+                    self.vocabDict[vocab] += 1
+                else:
+                    self.vocabDict.update({vocab: 1})
+        with open("vocabs.json", "w") as outfile:
+            json.dump(self.vocabDict, outfile)
+            outfile.close()
+
+    ## score sentence based on vocabs
+    ## ordering is not important
+    def score_q(self, q1, q2):
+        q1_vocabs = q1.split(" ")
+        q2_vocabs = q2.split(" ")
+        score = 0
+
+        for vocab in q1_vocabs:
+            if vocab in q2_vocabs:
+                score += self.score_vocab(vocab)
+
+        return score
